4104a84231112c435a6c0c38f5442859f226e1a3
manelcherni123 <chernimanel00@gmail.com>
2024-03-24 01:03:57 +0300

    ajout fichier sql

---
+++ prestataire.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 23, 2024 at 10:48 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prestataire`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fleuriste`
+--
+
+CREATE TABLE `fleuriste` (
+  `id_fleuriste` int(11) NOT NULL,
+  `nom` varchar(200) NOT NULL,
+  `mail` varchar(200) NOT NULL,
+  `num_tel` int(8) NOT NULL,
+  `localisasion` text NOT NULL,
+  `avis` text NOT NULL,
+  `prix` double NOT NULL,
+  `service` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fleuriste`
+--
+ALTER TABLE `fleuriste`
+  ADD PRIMARY KEY (`id_fleuriste`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
