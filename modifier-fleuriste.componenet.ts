59238ef739d27b3777d5041ee579118e6d4df853
FarahTouil <158171610+farahtouil@users.noreply.github.com>
2024-03-24 23:07:49 +0100

    Update modifier-fleuriste.componenet.ts

--- modifier-fleuriste.componenet.ts
+++ modifier-fleuriste.componenet.ts
@@ -17,13 +17,24 @@ export class ModifierFleuristeComponent {
   }
 
   modifierFleuriste() {
-    this.http.put<any>('http://127.0.0.1:3000/fleuriste.php?id_fleuriste=' + this.id_fleuriste,{
-    }).subscribe(response => {
-       console.log(response); // Traiter la réponse du serveur
-       this.annulerModification(); 
+    const updatedData = {
+    nom: this.nom,
+    lieu: this.lieu,
+    avis: this.avis,
+    prix: this.prix,
+    mail: this.mail,
+    numeroTelephone: this.numeroTelephone,
+    service: this.service,
+  };
+
+  // Make the HTTP PUT request with the updated data
+  this.http.put<any>('http://127.0.0.1:3000/fleuriste.php?id_fleuriste=' + this.id_fleuriste, updatedData)
+    .subscribe(response => {
+      console.log(response); // Handle server response
+      this.annulerModification(); 
     }, error => {
-       console.error(error); // Gérer les erreurs
-     });
+      console.error(error); // Handle errors
+    });
   }
 
   annulerModification() {
